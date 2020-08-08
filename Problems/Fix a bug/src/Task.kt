f3cb61268912c844081fbbf038c4e3158d89c3d0
d9nich <dan.galaiko@gmail.com>
2020-08-08 19:49:02 +0300

    I`ve learnt all about objects in Kotlin.

--- Problems/Fix a bug/src/Task.kt
+++ Problems/Fix a bug/src/Task.kt
@@ -1,4 +1,4 @@
-class Article(val name: String, val pages: Int, val author: String)
+data class Article(val name: String, val pages: Int, val author: String)
 
 fun getArticleByName(articles: Array<Article>, name: String): Article? {
     var index = -1
