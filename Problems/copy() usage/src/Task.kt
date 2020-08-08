bdf4fe5d2a8ee0d5d52e1c85fb475fc11fa811b8
d9nich <dan.galaiko@gmail.com>
2020-08-08 19:16:48 +0300

    I`ve learnt toString in Kotlin.

---
+++ Problems/copy() usage/src/Task.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+
+// do not change this data class
+data class Box(val height: Int, val length: Int, val width: Int)
+
+fun main(args: Array<String>) {
+    val input = Scanner(System.`in`)
+    // implement your code here
+}
